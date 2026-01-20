c09477aad5eca78903a6a8132dffb92c2bc320fd
Ozan Erel <ozanerell@hotmail.com>
2026-01-20 16:58:54 +0300

    İş başvurusu için hazırlanılan api için mvc tarafında service tarafı oluşturuldu.

---
+++ IK.MVCUI/Services/IJobApplicationApiService.cs
@@ -0,0 +1,10 @@
+﻿using IK.ENTITIES.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace IK.MVCUI.Services
+{
+    public interface IJobApplicationApiService
+    {
+        Task<bool> SendApplicationAsync(JobApplication jobApplication,IFormFile cvFile);
+    }
+}
---
+++ IK.MVCUI/Services/JobApplicationApiService.cs
@@ -0,0 +1,40 @@
+﻿using IK.ENTITIES.Models;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+
+namespace IK.MVCUI.Services
+{
+    public class JobApplicationApiService : IJobApplicationApiService
+    {
+        private readonly HttpClient _httpClient;
+        public JobApplicationApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<bool> SendApplicationAsync(JobApplication jobApplication, IFormFile cvFile)
+        {
+            using var content = new MultipartFormDataContent();
+
+            // Text alanları
+            content.Add(new StringContent(jobApplication.ApplicantName), "ApplicantName");
+            content.Add(new StringContent(jobApplication.Email), "Email");
+            content.Add(new StringContent(jobApplication.PhoneNumber), "PhoneNumber");
+            content.Add(new StringContent(jobApplication.PositionId.ToString()), "PositionId");
+            content.Add(new StringContent(jobApplication.PrivacyAccepted.ToString()), "PrivacyAccepted");
+
+            // CV
+            if (cvFile != null && cvFile.Length > 0)
+            {
+                var stream = cvFile.OpenReadStream();
+                var fileContent = new StreamContent(stream);
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue(cvFile.ContentType);
+
+                content.Add(fileContent, "CVFile", cvFile.FileName);
+            }
+
+            var response = await _httpClient.PostAsync("jobapplications", content);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
